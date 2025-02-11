bee3d01271c9ed219af1de55d600f2bf00c03f62
Evergood <arinmandri@gmail.com>
2025-02-11 09:32:20 +0900

    MonthType 클래스를 원시타입 byte로 대체

--- src/test/java/xyz/arinmandri/AppTest.java
+++ src/test/java/xyz/arinmandri/AppTest.java
@@ -22,22 +22,41 @@ public class AppTest
 	/**
 	 * 모든 음력 월의 1일의 양력날짜, 평윤, 대소 정보 가져오기
 	 */
+	@Test
 	public void scrap () {
 
-		for( int y = 1989 ; y < 2051 ; y++ ){
+		for( int y = 2004 ; y < 2006 ; y++ ){
+			System.out.print( "new YearData( LocalDate.of( " );
 			for( int m = 1 ; m < 13 ; m++ ){
 
 				List<Item> items = api.getFromLunDate( y, m, 1 );
 				for( Item item : items ){
 					LocalDate ld = LocalDate.of( item.getSolYear(), item.getSolMonth(), item.getSolDay() );
-					System.out.println( y + "-" + i( m ) + "(" + item.getLunLeapmonth() + "/" + item.getLunNday() + ")" + " --- " + ld );
+//					System.out.println( y + "-" + i( m ) + "(" + item.getLunLeapmonth() + "/" + item.getLunNday() + ")" + " --- " + ld );
+					
+					if(m==1) {
+						System.out.print( ld.getYear() + ", " + ld.getMonthValue() + ", " + ld.getDayOfMonth() + " ) , new byte[]{ " );
+					}
+
+					int isLeapMonth = switch( item.getLunLeapmonth() ){
+					case "평" -> 0;
+					case "윤" -> 2;
+					default-> throw new RuntimeException("??? ㄴㅇㄱ 평달도 윤달도 아닌 달이 있다!? 이건 당장 제보해야 돼.");
+					};
+
+					int isBigMonth = switch( item.getLunNday() ){
+					case 29 -> 0;
+					case 30 -> 1;
+					default-> throw new RuntimeException("??? ㄴㅇㄱ 대월도 소월도 아닌 달이 있다!? 이건 당장 제보해야 돼.");
+					};
+
+					System.out.print( ( isLeapMonth + isBigMonth ) + ", " );
 				}
 			}
-			System.out.println( "***" );
+			System.out.println( "} )," );
 		}
 	}
 
-	@Test
 	public void staticTest () {
 
 		LocalDate ld = LocalDate.of( 2036, 7, 23 );
@@ -51,7 +70,7 @@ public class AppTest
 	@Test
 	public void randomTest () {
 
-		int testSize = 1000;
+		int testSize = 100;
 
 		for( int i = 0 ; i < testSize ; i++ ){
 
