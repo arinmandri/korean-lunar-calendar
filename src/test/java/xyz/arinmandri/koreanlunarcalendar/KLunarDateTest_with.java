6f00a4faa555c161c920d93fc5805c552ae6b6bc
Evergood <arinmandri@gmail.com>
2025-04-25 21:47:31 +0900

    with 테스트 추가 (정상 경우만)

--- src/test/java/xyz/arinmandri/koreanlunarcalendar/KLunarDateTest_with.java
+++ src/test/java/xyz/arinmandri/koreanlunarcalendar/KLunarDateTest_with.java
@@ -2,10 +2,58 @@ package xyz.arinmandri.koreanlunarcalendar;
 
 import org.junit.jupiter.api.Test;
 
+
 public class KLunarDateTest_with extends KLunarDateTest
 {
 	@Test
-	public void test () {
-		System.out.println("으음... 고민중. 랜덤이 아니라 몇몇 경우를 뽑는 게 나을 듯.");
-	}
+	public void testWithYear () {
+		KLunarDate kd0, kd1;
+
+		kd0 = KLunarDate.of( YEAR_MIN, 1, false, 1 );
+		kd1 = kd0.withYear( YEAR_MAX );
+		checkValue( kd1, YEAR_MAX, 1, false, 1 );
+
+		kd0 = KLunarDate.of( 2004, 2, true, 1 );
+		kd1 = kd0.withYear( 2005 );
+		checkValue( kd1, 2005, 2, false, 1 );
+
+		kd0 = KLunarDate.of( 2010, 12, false, 30 );
+		kd1 = kd0.withYear( 2009 );
+		checkValue( kd1, 2009, 12, false, 30 );
+		kd1 = kd0.withYear( 2005 );
+		checkValue( kd1, 2005, 12, false, 29 );
+	}// TODO 안 되는 경우
+
+	@Test
+	public void testWithMonth () {
+		KLunarDate kd0, kd1;
+
+		kd0 = KLunarDate.of( 2004, 2, true, 11 );
+		for( int m = 1 ; m <= 12 ; m++ ){
+			if( m != 2 ){
+				kd1 = kd0.withMonth( m );
+				checkValue( kd1, 2004, m, false, 11 );
+			}
+			else{
+				kd1 = kd0.withMonth( m );
+				checkValue( kd1, 2004, m, true, 11 );
+			}
+		}
+	}// TODO 안 되는 경우
+
+	@Test
+	public void testWithDay () {
+		KLunarDate kd0, kd1;
+
+		kd0 = KLunarDate.of( 2004, 12, false, 1 );
+		for( int d = 1 ; d <= 29 ; d++ ){
+			kd1 = kd0.withDay( d );
+			checkValue( kd1, 2004, 12, false, d );
+		}
+		kd0 = KLunarDate.of( 2020, 12, false, 1 );
+		for( int d = 1 ; d <= 30 ; d++ ){
+			kd1 = kd0.withDay( d );
+			checkValue( kd1, 2020, 12, false, d );
+		}
+	}// TODO 안 되는 경우
 }
