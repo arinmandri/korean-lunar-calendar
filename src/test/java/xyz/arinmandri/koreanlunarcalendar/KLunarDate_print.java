ebb7df9b1155a35e1d1bc042505c5c8deb26007d
Evergood <arinmandri@gmail.com>
2025-04-28 02:24:25 +0900

    print: 1년 길이, 1갑자 길이 확인

--- src/test/java/xyz/arinmandri/koreanlunarcalendar/KLunarDate_print.java
+++ src/test/java/xyz/arinmandri/koreanlunarcalendar/KLunarDate_print.java
@@ -7,12 +7,10 @@ import org.junit.jupiter.api.Test;
 
 public class KLunarDate_print extends KLunarDateTest
 {
-	/**
-	 * 윤달들 확인
-	 */
+
 	@Test
 	public void printLeapMonths () {
-		System.out.println( "=== 윤달 목록 ===" );
+		printTitle( "윤달 목록" );
 		for( int c = 0 ; c < ydss.length ; c++ ){
 			for( int y0 = 0 ; y0 < ydss[c].length ; y0++ ){
 				int yd = ydss[c][y0];
@@ -22,6 +20,36 @@ public class KLunarDate_print extends KLunarDateTest
 					System.out.println( year + "-" + leapMonth );
 				}
 			}
+		}
+	}
+
+	@Test
+	public void printYearLengths () {
+		printTitle( "1년 길이" );
+
+		KLunarDate kd = KLunarDate.of( YEAR_MIN, 1, 1 );
+		int epochDay0 = kd.toEpochDayInt();
+		for( int y = YEAR_MIN + 1 ; y <= YEAR_MAX ; y++ ){
+			kd = KLunarDate.of( y, 1, 1 );
+			int epochDay1 = kd.toEpochDayInt();
+			int yl = epochDay1 - epochDay0;
+			System.out.println( ( y - 1 ) + " - " + yl );
+			epochDay0 = epochDay1;
+		}
+	}
+
+	@Test
+	public void printCycleLengths () {
+		printTitle( "1갑자 길이" );
+
+		KLunarDate kd = KLunarDate.of( YEAR_MIN, 1, 1 );
+		int epochDay0 = kd.toEpochDayInt();
+		for( int y = YEAR_MIN + CYCLE_SIZE ; y <= YEAR_MAX ; y += CYCLE_SIZE ){
+			kd = KLunarDate.of( y, 1, 1 );
+			int epochDay1 = kd.toEpochDayInt();
+			int yl = epochDay1 - epochDay0;
+			System.out.println( ( y - CYCLE_SIZE ) + " - " + yl );
+			epochDay0 = epochDay1;
 		}
 	}
 }
