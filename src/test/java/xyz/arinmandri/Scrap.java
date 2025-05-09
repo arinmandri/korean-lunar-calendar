0359db1ff329766a4c08ab416078b525b4810c93
Evergood <arinmandri@gmail.com>
2025-05-09 23:31:12 +0900

    scrap2: getSpcifyLunCalInfo 를 이용하여 정보 가져오도록 수정 (KASI API 호출 수 확 줄임)

--- src/test/java/xyz/arinmandri/Scrap.java
+++ src/test/java/xyz/arinmandri/Scrap.java
@@ -2,7 +2,10 @@ package xyz.arinmandri;
 
 import static xyz.arinmandri.koreanlunarcalendar.Ganji.CYCLE_SIZE;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 import xyz.arinmandri.kasiapi.ApiService;
 import xyz.arinmandri.kasiapi.Item;
@@ -12,15 +15,159 @@ public class Scrap
 {
 	ApiService api = ApiService.getInstance();
 
-	final int YEAR_BASE = 1864;// KLunarDate.YEAR_BASE
-	final int year_min = YEAR_BASE;
-	final int year_max = YEAR_BASE + CYCLE_SIZE * 10;
+	/**
+	 * 
+	 * 모든 음력 월의 1일의 양력날짜, 평윤, 대소 정보 가져오기
+	 * KASI API의 getSpcifyLunCalInfo로써 모든 년의 1월을 한 번에 조회, 모든 년의 2월을 한 번에 조회, ...
+	 * KLunarDate.ydss 소스코드에 붙여넣을 형식으로 출력한다.
+	 */
+	@Test
+	public void scrap2 () {
+		final int YEAR_MIN = 1391;
+		final int YEAR_MAX = 2050;
+		final int EPOCH_0_JDAY = 2440588;
+
+		YearData[] yds = new YearData[YEAR_MAX - YEAR_MIN + 1];
+		for( int i = 0 ; i < yds.length ; i++ )
+		    yds[i] = new YearData();
+
+		List<Integer> epochDays = new ArrayList<>();
+
+		//// 각 해의 첫달: 율리우스 적일
+		{
+			List<Item> items = api.getSpcifyLunCalInfo( YEAR_MIN, YEAR_MAX, 1, 1, false );
+			for( Item item : items ){
+				YearData yd = yds[item.getLunYear() - YEAR_MIN];
+				yd.setJDay( item.getSolJd() );
+			}
+		}
+
+		//// 윤달정보 및 각 월의 대소
+		for( int m = 1 ; m <= 12 ; m += 1 ){
+			List<Item> items = api.getSpcifyLunCalInfo( YEAR_MIN, YEAR_MAX, m, 1, null );
+			for( Item item : items ){
+				YearData yd = yds[item.getLunYear() - YEAR_MIN];
+				yd.pushIsBigMonth( item.getLunNday() == 30 );
+				if( item.getLunLeapmonth().equals( "윤" ) )
+				    yd.setLeapMonth( item.getLunMonth() );
+			}
+		}
+		if( YEAR_MAX >= 2050 ){// 2050년만 각월대소 직접 입력(KASI API 지원이 중간에 잘려서)
+			YearData yd2050 = yds[2050 - YEAR_MIN];
+			yd2050.isBigMonths = new boolean[]{ true, false, false, true, false, true, false, true, true, false, true, true, false, };
+		}
+
+		{
+			int y = Integer.MIN_VALUE;
+			int c = 0;
+			while( true ){// 주기
+				System.out.println( "\t{" );
+
+				int y_c = YEAR_MIN + c * 60;
+				int cJDay = yds[y_c - YEAR_MIN].getJDay();
+
+				int cEpochDay = cJDay - EPOCH_0_JDAY;
+				epochDays.add( cEpochDay );
+
+				for( int cd = 0 ; cd < 60 ; cd += 10 ){// 10년 (10년마다 1줄씩 출력)
+					System.out.print( "\t\t" );
+
+					int y_cd = y_c + cd;
+					for( int dy = 0 ; dy < 10 ; dy += 1 ){// 해
+						y = y_cd + dy;
+						if( y > YEAR_MAX ) break;
+
+						YearData yd = yds[y - YEAR_MIN];
+						System.out.print( String.format( "0x%08X", yd.export( cJDay ) ) );
+						System.out.print( ", " );
+					}
+					System.out.println( "// " + y_cd );
+					if( y >= YEAR_MAX ) break;
+				}
+				System.out.println( "\t}," );
+				if( y >= YEAR_MAX ) break;
+				c += 1;
+			}
+		}
+
+		System.out.println( "----------------------------------------" );
+
+		System.out.println( "static final int[] epochDays = {" );
+		for( int c = 0 ; c < epochDays.size() ; c++ ){
+			System.out.println( "\t" + epochDays.get( c ) + ",// " + ( YEAR_MIN + c * 60 ) );
+		}
+		System.out.println( "\t" + 29626 + ",// 마지막 값은 지원범위 판별용// 음력 2050-12-29의 다음 날에 해당. 한국천문연구원 API에서 지원범위의 막날은 2050-11-18이지만 2050-11이 대월, 30일까지 있음은 알 수 있으므로 이 라이브러리의 지원범위는 2050-12-29까지는 늘려짐.(이 해에 윤달이 이미 나왔고 그 이전의 두 달이 대월이므로 2050-12-29의 다음 날은 아마 2051-01-01일 거 같지만)" );
+		System.out.println( "};" );
+	}
+
+	private class YearData
+	{
+		private int jDay = 0;// 이 해의 첫날의 율리우스적일
+		private int leapMonth = 0xF;
+		private boolean[] isBigMonths = new boolean[13];
+		private int m0 = 0;// 이 다음엔 몇 번째 월 정보를 채울지 (0부터 시작)
+
+		void setJDay ( int _jDay ) {
+			if( jDay != 0 )
+			    throw new RuntimeException( "jDay 중복설정" );
+			jDay = _jDay;
+		}
+
+		int getJDay () {
+			return jDay;
+		}
+
+		void setLeapMonth ( int _leapMonth ) {
+			if( leapMonth != 0xF )
+			    throw new RuntimeException( "leapMonth 중복설정" );
+			leapMonth = _leapMonth;
+		}
+
+		void pushIsBigMonth ( boolean isBigMonth ) {
+			if( m0 >= 13 )
+			    throw new RuntimeException( "isBigMonths 13개 넘음" );
+			isBigMonths[m0++] = isBigMonth;
+		}
+
+		int export ( int cJDay ) {// cJDay: 이 주기의 첫날의 율리우스적일
+			int yd = 0;
+
+			// 15비트: 이 주기의 첫날과 이 년의 첫 날의 날짜 차이 (일 단위)
+			int yOffset = jDay - cJDay;
+			yd |= yOffset << 17;
+
+			// 4비트: 몇 월에 윤달이 있나
+			yd |= leapMonth << 13;
+
+			// 13비트: 각 월의 대소(0:소 1:대) (0번째 달부터 12번째 달까지 윤달도 똑같이 한 달로 취급, 1의자리부터 1월)
+			for( int m0 = 0 ; m0 < 13 ; m0 += 1 ){
+				if( isBigMonths[m0] )
+				    yd |= 1 << m0;
+			}
+
+			return yd;
+		}
+
+		public String toString () {
+			String str = "jDay(" + jDay + ") 윤" + ( leapMonth == 0xF ? "-" : leapMonth ) + " / ";
+			for( boolean b : isBigMonths ){
+				str += b ? "1" : "0";
+			}
+			return str;
+		}
+	}
 
 	/**
 	 * 모든 음력 월의 1일의 양력날짜, 평윤, 대소 정보 가져오기
+	 * KASI API의 getFromLunDate로써 API 한 번에 한 달씩 조회한다.
+	 * KLunarDate.ydss 소스코드에 붙여넣을 형식으로 출력한다.
 	 */
 //	@Test
 	public void scrap () {
+
+		final int YEAR_BASE = 2049;// KLunarDate.YEAR_BASE
+		final int year_min = YEAR_BASE;
+		final int year_max = YEAR_BASE + CYCLE_SIZE * 10;
 
 		final int leapMonthNone = 0xF;
 
@@ -120,14 +267,10 @@ public class Scrap
 		System.out.print( "\n\n" );
 		System.out.println( cDaySB );
 		System.out.println( "jDays 마지막 항목은 손수 입력... (" + jDayC0 + ")" );
-	}
 
-	/*
-	 * 년도별 일수 긁어오기
-	 */
-//	@Test
-	public void jDays () {
-
+		/*
+		 * 년도별 일수 긁어오기
+		 */
 		int jDay = 0;
 		for( int y = year_min ; y <= 2049 ; y++ ){
 			List<Item> items = api.getFromLunDate( y, 1, 1 );
