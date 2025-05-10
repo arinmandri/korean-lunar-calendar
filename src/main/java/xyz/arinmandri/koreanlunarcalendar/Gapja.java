885b4296507ec8c413ebcca65b07d96401bedf90
Evergood <arinmandri@gmail.com>
2025-05-10 23:56:05 +0900

    Gapja 기능 구현: 단순히 baseUnit의 60배일 뿐이다.

--- src/main/java/xyz/arinmandri/koreanlunarcalendar/Gapja.java
+++ src/main/java/xyz/arinmandri/koreanlunarcalendar/Gapja.java
@@ -1,20 +1,32 @@
 package xyz.arinmandri.koreanlunarcalendar;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalUnit;
 
 
 public enum Gapja implements TemporalUnit {
-	DAY_GAPJAS   (Duration.ofSeconds( 60 * 60 * 60 * 24 ), false),// 60일
-	MONTH_GAPJAS (Duration.ofSeconds( 153_084_657L ), true),// 60개월 // 19년마다 7개 윤달이라 치자. 19년마다 19*12+7개월. 1년마다 ((19*12+7)/19)개월 ... 60 * 31_556_925L / ( ( 19 * 12 + 7 ) / 19.0 )
-	YEAR_GAPJAS  (Duration.ofSeconds( 60 * 31_556_925L ), true),// 60년 // 1년 평균 Tropical Year 365.2421896698일 * 하루 86400초 = 1년 31,556,925.18747072초 https://en.wikipedia.org/wiki/Tropical_year
+	DAY_GAPJAS (
+	        ChronoUnit.DAYS,
+	        Duration.ofSeconds( 60 * 60 * 60 * 24 ),// 60일
+	        false),
+	MONTH_GAPJAS (
+	        LunarMonthUnit.LMONTH_BUNDLES,
+	        Duration.ofSeconds( 31556952L * 5 ),// 60개월
+	        true),
+	YEAR_GAPJAS (
+	        ChronoUnit.YEARS,
+	        Duration.ofSeconds( 60 * 31_556_925L ),// 60년 // 1년 평균 Tropical Year 365.2421896698일 * 하루 86400초 = 1년 31,556,925.18747072초 https://en.wikipedia.org/wiki/Tropical_year
+	        true),
 	;
 
+	private final TemporalUnit baseUnit;// 이거는 이거의 60배
 	private final Duration duration;
 	private final boolean isDurationEstimated;
 
-	private Gapja( Duration duration , boolean isDurationEstimated ) {
+	private Gapja( TemporalUnit baseUnit , Duration duration , boolean isDurationEstimated ) {
+		this.baseUnit = baseUnit;
 		this.duration = duration;
 		this.isDurationEstimated = isDurationEstimated;
 	}
@@ -41,14 +53,16 @@ public enum Gapja implements TemporalUnit {
 
 	@Override
 	public < R extends Temporal > R addTo ( R temporal , long amount ) {
-		// TODO Auto-generated method stub
-		return null;
+		return baseUnit.addTo( temporal, amount * Ganji.CYCLE_SIZE );
 	}
 
 	@Override
 	public long between ( Temporal temporal1Inclusive , Temporal temporal2Exclusive ) {
-		// TODO Auto-generated method stub
-		return 0;
+		if( temporal1Inclusive.getClass() != temporal2Exclusive.getClass() ){
+			return temporal1Inclusive.until( temporal2Exclusive, this );
+		}
+
+		return baseUnit.between( temporal1Inclusive, temporal2Exclusive ) / Ganji.CYCLE_SIZE;
 	}
 
 }
