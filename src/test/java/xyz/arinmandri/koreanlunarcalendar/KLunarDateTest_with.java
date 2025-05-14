6b80db6609e5163beecf652f3cefbcaed05cfb5f
Evergood <arinmandri@gmail.com>
2025-05-14 19:10:59 +0900

    KLunarDateTest_with: 윤달

--- src/test/java/xyz/arinmandri/koreanlunarcalendar/KLunarDateTest_with.java
+++ src/test/java/xyz/arinmandri/koreanlunarcalendar/KLunarDateTest_with.java
@@ -1,6 +1,7 @@
 package xyz.arinmandri.koreanlunarcalendar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 import java.time.temporal.ChronoField;
 import java.time.temporal.ValueRange;
@@ -109,7 +110,51 @@ public class KLunarDateTest_with extends KLunarDateTest
 			System.out.println( i );
 			throw e;
 		}
-	}// TODO 윤달은
+	}
+
+	@Test
+	public void testWithLeapMonth () {
+		printTitle( "with leapMonth" );
+		/*
+		 * 모든 년월에 대해 윤달이 있으면 withMonthLeap로 윤달을 만들어보고
+		 * 윤달이 없는 달이면 윤달로 바꿨을 때 에러가 나는지 확인
+		 */
+		int y_min = YEAR_MIN;
+		int y_max = YEAR_MAX;
+		ValueRange mr = LunarMonthField.MONTH_N.range();
+		int m_min = Math.toIntExact( mr.getMinimum() );
+		int m_max = Math.toIntExact( mr.getMaximum() );
+		int y = -9999, m = -9999;
+
+		try{
+			for( y = y_min ; y <= y_max ; y += 1 ){
+				for( m = m_min ; m < m_max ; m += 1 ){
+					boolean isLeapMonth = KLunarChronology.INSTANCE.isLeapMonth( y, m );
+					if( isLeapMonth ){
+						KLunarDate kd = KLunarDate.of( y, m, 1 );
+						assertEquals(
+						        KLunarDate.of( y, m, true, 1 ),
+						        kd.withMonthLeap( true ) );
+						assertEquals(
+						        KLunarDate.of( y, m, false, 1 ),
+						        kd.withMonthLeap( true ).withMonthLeap( false ) );
+					}
+					else{
+						final int yy = y;
+						final int mm = m;
+						assertThrowsExactly( NonexistentDateException.class, ()-> {
+							KLunarDate.of( yy, mm, 1 ).withMonthLeap( true );
+						} );
+					}
+				}
+			}
+		}
+		catch( Throwable e ){
+			System.out.println( "DOOM: " + e.getMessage() );
+			System.out.println( y + "-" + m );
+			throw e;
+		}
+	}
 
 	@Test
 	public void testWithDay () {
