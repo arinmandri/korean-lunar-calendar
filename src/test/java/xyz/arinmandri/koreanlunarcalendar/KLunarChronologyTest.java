48e850533f8299d389a4cb90847516cb0f104582
Evergood <arinmandri@gmail.com>
2025-05-14 18:44:18 +0900

    KLunarChronology.isLeapYear, KLunarChronology.isLeapMonth 테스트 추가

--- src/test/java/xyz/arinmandri/koreanlunarcalendar/KLunarChronologyTest.java
+++ src/test/java/xyz/arinmandri/koreanlunarcalendar/KLunarChronologyTest.java
@@ -1,8 +1,10 @@
 package xyz.arinmandri.koreanlunarcalendar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.time.chrono.Chronology;
+import java.time.temporal.ValueRange;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +19,55 @@ public class KLunarChronologyTest extends KLunarDateTest
 		assertEquals( KLunarChronology.INSTANCE, c1 );
 		Chronology c2 = Chronology.of( "KoreanLunar" );// src/main/resources/META-INF/services/java.time.chrono.Chronology
 		assertEquals( c1, c2 );
+	}
+
+	@Test
+	public void testIsLeapYear () {
+		printTitle( "KLunarChronology.isLeapYear" );
+		int min = YEAR_MIN - 10;
+		int max = YEAR_MAX + 10;
+
+		for( int y = min ; y <= max ; y += 1 ){
+			boolean isLeapYear = KLunarChronology.INSTANCE.isLeapYear( y );
+			try{
+				if( y < YEAR_MIN )
+				    assertFalse( isLeapYear );
+				else if( y > YEAR_MAX )
+				    assertFalse( isLeapYear );
+				else{
+					KLunarDate kd = KLunarDate.of( y, 1, 1 );
+					assertEquals( kd.isLeapYear(), isLeapYear );
+				}
+			}
+			catch( Throwable e ){
+				System.out.println( "DOOM: " + y + " - " + isLeapYear );
+				throw e;
+			}
+		}
+	}
+
+	@Test
+	public void testIsLeapMonth () {
+		printTitle( "KLunarChronology.isLeapMonth" );
+		int y_min = YEAR_MIN - 10;
+		int y_max = YEAR_MAX + 10;
+		ValueRange mr = LunarMonthField.MONTH_N.range();
+		int m_min = Math.toIntExact( mr.getMinimum() );
+		int m_max = Math.toIntExact( mr.getMaximum() );
+
+		for( int y = y_min ; y <= y_max ; y += 1 ){
+			Integer leapMonth = leapMonthsMap.get( y );
+			for( int m = m_min ; m < m_max ; m += 1 ){
+				try{
+					boolean isLeapMonth = KLunarChronology.INSTANCE.isLeapMonth( y, m );
+					boolean isLeapMonth0 = leapMonth != null && leapMonth == m;
+					assertEquals( isLeapMonth0, isLeapMonth );
+				}
+				catch( Throwable e ){
+					System.out.println( "DOOM: " + y + "-" + m );
+					throw e;
+				}
+			}
+		}
 	}
 }
